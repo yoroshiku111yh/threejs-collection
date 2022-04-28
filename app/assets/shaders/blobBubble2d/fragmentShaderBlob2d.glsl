e9f1b64ba438a982b8cad64da8e8212bd4071e55
Minh Vu <minh.vu@thinkclimax.com>
2022-04-28 12:30:48 +0700

    optimize

--- app/assets/shaders/blobBubble2d/fragmentShaderBlob2d.glsl
+++ app/assets/shaders/blobBubble2d/fragmentShaderBlob2d.glsl
@@ -17,11 +17,28 @@ uniform float uRadius;
 uniform bool isMouse;
 uniform float uAlpha;
 
-
 varying vec2 vUv;
 
+vec4 mouseGooey(in vec4 color, in bool isMouse, in vec2 uv, in float uSpikes, in float uTime, in vec2 uMousePos) {
+    if(isMouse) { // use in research
+        // generate cursor blob noise (slow down uTime)
+        float mouseBlobNoise = snoise3(vec3(uv.x * uSpikes, uv.y * uSpikes, uTime * 0.3));
+
+        float mouseRadius = uMouseRadius;
+        // increase mouse radius based on distance from center vec2(0.5)
+        mouseRadius = mouseRadius * (1.0 + smoothstep(0.15, 0.45, distance(uMousePos, vec2(0.5))));
+
+        // apply noise to mouseRadius
+        float cursorRadius = map(mouseBlobNoise, 0.0, 1.0, mouseRadius - (mouseRadius * 0.2), mouseRadius);
+
+        // draw cursor circle blob at mouse pos
+        vec3 cursorColor = mix(color.rgb, uColor, circle(uv, cursorRadius, uMousePos));
+        color = vec4(cursorColor, 1.0);
+    }
+    return color;
+}
+
 void main() {
-    float mouseRadius = uMouseRadius;
     vec2 uv = vUv;
     vec2 coverUv = cover(uv, uPlaneSize, uImageSize);
 
@@ -29,27 +46,13 @@ void main() {
     vec2 posTexture = coverUv + vec2(uTextureMoveX, uTextureMoveY);
     vec4 texture = texture2D(uTexture, posTexture);
     vec4 color = texture;
-    if(isMouse) { // use in research
-        // generate cursor blob noise (slow down uTime)
-        float mouseBlobNoise = snoise3(vec3(uv.x * uSpikes, uv.y * uSpikes, uTime * 0.3));
-
-    // increase mouse radius based on distance from center vec2(0.5)
-        mouseRadius = mouseRadius * (1.0 + smoothstep(0.15, 0.45, distance(uMousePos, vec2(0.5))));
-
-    // apply noise to mouseRadius
-        float cursorRadius = map(mouseBlobNoise, 0.0, 1.0, mouseRadius - (mouseRadius * 0.2), mouseRadius);
-
-    // draw cursor circle blob at mouse pos
-        vec3 cursorColor = mix(color.rgb, uColor, circle(uv, cursorRadius, uMousePos));
-        color = vec4(cursorColor, 1.0);
-    }
-
+    //color = mouseGooey(color, isMouse, uv, uSpikes, uTime, uMousePos);
     // use another circle blob as mask on the color's alpha channel (black === alpha of 0)
     float imageBlobNoise = snoise3(vec3(uv.x * uSpikes, uv.y * uSpikes, uTime * 1.0));
     float radius = map(imageBlobNoise, 0.0, 1.0, uRadius * 0.9, uRadius);
     float mask = circle(uv, radius);
     color.a = mask;
-    if(color.a < 1.0){
+    if(color.a < 1.0) {
         discard;
     }
 
