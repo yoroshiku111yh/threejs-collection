c8dddb25cd7a48e14cc70db7023c0fe01535f011
Minh Vu <minh.vu@thinkclimax.com>
2022-09-29 14:02:25 +0700

    gradient mix

--- app/assets/shaders/helper/gradientMix2Colors.glsl
+++ app/assets/shaders/helper/gradientMix2Colors.glsl
@@ -8,16 +8,27 @@ mat2 rotate2d(float value){
   return mat2(cos(value), -sin(value), sin(value), cos(value));
 }
 
+// vec3 gradientMix2Colors(vec2 st, float tick, vec3 col1, vec3 col2, vec2 pos){
+//   vec3 c1 = col1;
+//   vec3 c2 = col2;
+
+//   st -= pos;
+//   st *= scale(vec2(3.8));
+//   st *= rotate2d(tick * PI);
+//   st += pos;
+
+//   return mix(c1, c2, st.x);
+// }
+
 vec3 gradientMix2Colors(vec2 st, float tick, vec3 col1, vec3 col2, vec2 pos){
   vec3 c1 = col1;
   vec3 c2 = col2;
-
   st -= pos;
-  st *= scale(vec2(3.8));
   st *= rotate2d(tick * PI);
+  float mixValue = distance(st, vec2(0, 1));
+  vec3 col = mix(c1, c2, mixValue);
   st += pos;
-
-  return mix(c1, c2, st.x);
+  return col;
 }
 
 #pragma glslify: export(gradientMix2Colors);
